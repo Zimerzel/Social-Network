437c37f3550b6dae88b77c7b3691bb4ca365428c
Imerzel <zach.imerzel@gmail.com>
2021-11-01 17:45:29 -0500

    added navbar, login, signup, home components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import Home from './pages/Home'
+
+const http = createHttpLink({
+  uri: "/graphql"
+})
+
+const auth = setContext((_,{headers}) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token? `Bearer ${token}` : "",
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: auth.concat(http),
+  cache: new InMemoryCache(),
+  uri: "/graphql"
+})
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
